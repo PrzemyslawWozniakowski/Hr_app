9ddb55a963149898f9964a0e6c03355f57612652
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2019-10-31 17:07:24 +0100

    Job offer list added

---
+++ hr app/hr app/Controllers/Job_OfferController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using hr_app.Models;
+
+namespace hr_app.Controllers
+{
+    public class Job_OfferController : Controller
+    {
+        private readonly Job_OfferContext _context;
+
+        public Job_OfferController()
+        {
+            _context = new Job_OfferContext();
+        }
+
+        public IActionResult Job_offers()
+        {
+            List<Job_OfferModel> list = _context.job_Offers;
+            List<Job_OfferViewModel> listvm = new List<Job_OfferViewModel>();
+            foreach (var el in list)
+            {
+                listvm.Add(new Job_OfferViewModel(el));
+            }
+            return View(listvm);
+        }
+
+   
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ hr app/hr app/Data/Job_OfferContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hr_app.Models
+{
+    public class Job_OfferContext 
+    {
+        public Job_OfferContext()
+        {
+            job_Offers = new List<Job_OfferModel>(new Job_OfferModel[] { new Job_OfferModel(1,"CEO", new decimal(1000), new decimal(15020), new DateTime(2019, 10, 24), "Warsaw", "Some job",new DateTime(2019, 10, 27)),
+                                                                         new Job_OfferModel(2,"DevOps", new decimal(15000), new decimal(35020), new DateTime(2019, 10, 24), "Warsaw", "Some job",new DateTime(2019, 10, 27)),
+                                                                           new Job_OfferModel(3,"Junior .Net Programmer", new decimal(15000), new decimal(15020), new DateTime(2019, 10, 24), "Warsaw", "Some job",new DateTime(2019, 10, 29)) });
+        }
+        public List<Job_OfferModel> job_Offers { get; set; }
+    }
+}
