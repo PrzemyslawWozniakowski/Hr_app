47a217120a1a4f8f16ed2acf8e7092e77230d130
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2019-11-04 11:47:10 +0100

    Added JobOffer controller  #60

---
+++ hr app/Controllers/JobOfferController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using hr_app.Models;
+using hr_app.EntityFramework;
+
+namespace hr_app.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class JobOfferController : Controller
+    {
+        //private readonly JobOfferContext _context;
+
+        //public JobOfferController()
+        //{
+        //    _context = new JobOfferContext();
+        //}
+
+        private readonly DataContext _context;
+
+        public JobOfferController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery(Name = "search")] string searchString)
+        {
+            if (string.IsNullOrEmpty(searchString))
+                return View(await _context.JobOffers.Include(x => x.Company).ToListAsync());
+
+            List<JobOffer> searchResult = await _context.JobOffers.Include(x => x.Company).Where(o => o.JobTitle.Contains(searchString)).ToListAsync();
+            List<JobOfferIndexView> result = new List<JobOfferIndexView>();
+            //foreach(var el in searchResult)
+            //{
+            //    result.Add(new JobOfferIndexView(el));
+            //}
+            return View(searchResult);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest($"id shouldn't not be null");
+            }
+            var offer = await _context.JobOffers.FirstOrDefaultAsync(x => x.Id == id.Value);
+            if (offer == null)
+            {
+                return NotFound($"offer not found in DB");
+            }
+
+            return View(offer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(JobOffer model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var offer = await _context.JobOffers.FirstOrDefaultAsync(x => x.Id == model.Id);
+            offer.JobTitle = model.JobTitle;
+            _context.Update(offer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", new { id = model.Id });
+        }
+        [HttpPost]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest($"id should not be null");
+            }
+
+            _context.JobOffers.Remove(new JobOffer() { Id = id.Value });
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            var model = new JobOfferCreateView
+            {
+                Companies = await _context.Companies.ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(JobOfferCreateView model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Companies = await _context.Companies.ToListAsync();
+                return View(model);
+            }
+
+            JobOffer jo = new JobOffer
+            {
+                CompanyId = model.CompanyId,
+                Description = model.Description,
+                JobTitle = model.JobTitle,
+                Location = model.Location,
+                SalaryFrom = model.SalaryFrom,
+                SalaryTo = model.SalaryTo,
+                ValidUntil = model.ValidUntil,
+                Created = DateTime.Now
+            };
+
+            await _context.JobOffers.AddAsync(jo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var offer = await _context.JobOffers.Include(x => x.Company).FirstOrDefaultAsync(x => x.Id == id);
+            return View(offer);
+        }
+    }
+}
