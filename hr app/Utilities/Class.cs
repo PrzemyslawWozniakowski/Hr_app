63ad0c33a15b14a9fc02e207d729ef5b3fbe3516
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2019-11-04 12:05:06 +0100

    Added aditional validation for creating job offer and a viewmodel #65

---
+++ hr app/Utilities/Class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using hr_app.Models;
+
+namespace hr_app.Utilities
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SalaryCorrectRangeAttributeAttribute : ValidationAttribute
+    {
+        public bool ValidateSalaryFrom { get; set; }
+        public bool ValidateSalaryTo { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = validationContext.ObjectInstance as JobOffer;
+
+            if (model != null)
+            {
+                if (model.SalaryFrom > model.SalaryTo)
+                {
+                    return new ValidationResult(string.Empty);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DateCorrectRangeAttribute : ValidationAttribute
+    {
+     
+        public bool ValidateValidUntil { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = validationContext.ObjectInstance as JobOffer;
+
+            if (model != null)
+            {
+                if ( model.ValidUntil < DateTime.Now.Date && ValidateValidUntil)
+                {
+                    return new ValidationResult(string.Empty);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
+
+
