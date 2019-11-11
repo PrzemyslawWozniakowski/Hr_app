0c7b06a1de9c47a510427d1a71f38e53ff240d58
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2019-11-12 00:06:56 +0100

    Added view models for Edit and Details views

---
+++ hr app/Models/JobOfferDetailsView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hr_app.Models
+{
+    public class JobOfferDetailsView
+    {
+        public JobOfferDetailsView(JobOffer offer)
+        {
+            Id = offer.Id;
+            JobTitle = offer.JobTitle;
+            Company = offer.Company;
+            SalaryFrom = offer.SalaryFrom;
+            SalaryTo = offer.SalaryTo;
+            Created = offer.Created;
+            Location = offer.Location;
+            Description = offer.Description;
+            ValidUntil = offer.ValidUntil;
+        }
+
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public Company Company { get; set; }
+        public decimal? SalaryFrom { get; set; }
+        public decimal? SalaryTo { get; set; }
+        public DateTime Created { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public DateTime? ValidUntil { get; set; }
+    }
+}
