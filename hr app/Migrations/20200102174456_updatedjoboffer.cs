9e7a21748c9b89981239403f0223ffa2428cc0e1
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2020-01-02 18:56:11 +0100

    updated job offer

---
+++ hr app/Migrations/20200102174456_updatedjoboffer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hr_app.Migrations
+{
+    public partial class updatedjoboffer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "JobOffers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "JobOffers");
+        }
+    }
+}
--- hr app/Models/User.cs
+++ hr app/Models/User.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Enums;
 namespace hr_app.Models
 {
-    public class AUser
+    public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public Role Role { get; set; }
     }
 }
