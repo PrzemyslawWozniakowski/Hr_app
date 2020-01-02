a159fc7af392d6ba6eaa997eea020184c62e238d
przemyslawwozniakowski <przemyslawwozniakowski@gmail.com>
2020-01-02 19:25:01 +0100

    updated job offer

--- hr app/Migrations/20200102175906_updatedjobapp.Designer.cs
+++ hr app/Migrations/20200102175906_updatedjobapp.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using hr_app.EntityFramework;
 
 namespace hr_app.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200102175906_updatedjobapp")]
+    partial class updatedjobapp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ hr app/Migrations/20200102175906_updatedjobapp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hr_app.Migrations
+{
+    public partial class updatedjobapp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "JobApplications",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "JobApplications");
+        }
+    }
+}
